078a1b92e1ef5fe4bf58caa5d45a13c4e905af02
Yony Calsin <helloyonycalsin@gmail.com>
2023-01-21 16:40:35 -0500

    feat: udpate the footer component

--- src/layouts/main-layout/containers/footer/footer.tsx
+++ src/layouts/main-layout/containers/footer/footer.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Box, Container, Link, Text } from '@chakra-ui/react'
+import { Box, Container, Link, Text, VStack } from '@chakra-ui/react'
 
 function Footer() {
   return (
@@ -12,16 +12,21 @@ function Footer() {
       }}
     >
       <Container>
-        <Text textAlign="center" fontWeight="bold" fontSize="lg">
-          Created by{' '}
-          <Link
-            href="https://www.yonycalsin.com/?utm_source=ms_tooly&utm_medium=footer&utm_campaign=author_attribution"
-            target="_blank"
-            textColor="secondary.500"
-          >
-            Yony Calsin
-          </Link>
-        </Text>
+        <VStack>
+          <Text textAlign="center" fontWeight="bold" fontSize="lg">
+            Created by{' '}
+            <Link
+              href="https://www.yonycalsin.com/?utm_source=ms_tooly&utm_medium=footer&utm_campaign=author_attribution"
+              target="_blank"
+              textColor="secondary.500"
+            >
+              Yony Calsin
+            </Link>
+          </Text>
+          <Text textAlign="center" fontSize="ms" textColor="gray.500">
+            ©2023 Yony Calsin - All Rights Reserved
+          </Text>
+        </VStack>
       </Container>
     </Box>
   )
